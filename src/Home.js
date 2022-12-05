361f11c9c737fa6c9badb34383d19488e23eca33
Dumindu Lakshan <it21266096@my.sliit.lk>
2022-12-05 14:05:02 +0530

    add new files

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+const Home = () =>{
+    return(
+        <div className="home">
+            <h1 style={{
+                color: "red",
+                fontSize: "3rem",
+                textAlign: "center"
+            }}>Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar= () => {
+    return (
+        <nav className="navbar">
+            <h1>Nav Bar</h1>
+            .<div className="links">
+                <li><a href="#">Home</a></li>
+                <li><a href="https://www.google.com/" target="_blank">Blog</a></li>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
