385e1854f6cabf2afccabb3d077a497685de6444
Dumindu Lakshan <it21266096@my.sliit.lk>
2022-12-05 14:27:40 +0530

    home updated

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,17 @@
 const Home = () =>{
+
+    const clickButton = () =>{
+        alert("Hello React Js");
+    }
+
+    const clickAgain = (name) => {
+        alert("Hello " + name);
+    }
+
+    const clickEvent = (name,e) => {
+        alert("Event: " + e.target, "Object: " + name);
+    }
+
     return(
         <div className="home">
             <h1 style={{
@@ -6,6 +19,9 @@ const Home = () =>{
                 fontSize: "3rem",
                 textAlign: "center"
             }}>Home Page</h1>
+            <button onClick={clickButton}>Click Alert #01</button>
+            <button onClick={() => clickAgain("React")}>Click Alert #02</button>
+            <button onClick={(e) => clickEvent("Python",e)}>Click Alert #03</button>
         </div>
     );
 }
