ffe076cd3f1745dd70da5d7c26d24e9fceed7719
Dumindu Lakshan <it21266096@my.sliit.lk>
2022-12-07 19:46:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+// import React from 'react';
+
 import Card from './components/Card';
 
 const App = () => {
   return (
 
     <div className="App">
-      <Navbar/>
-      <Hero/>
-      <Card/>
+      <Card
+        image="./images/cat1.jpg"
+        name="Mr. Doggy"
+        phone="(+94) 21-234321"
+        email="Mr.Doggy"
+      />
+      <Card
+        image="./images/cat2.jpg"
+        name="Mr. Kitty"
+        phone="(+94) 21-234321"
+        email="Mr.Doggy"
+      />
+      <Card
+        image="./images/cat2.jpg"
+        name="Mr. Meow"
+        phone="(+94) 21-234321"
+        email="Mr.Doggy"
+      />
+      <Card
+        image="./images/cat1.jpg"
+        name="Mr. Doggy"
+        phone="(+94) 21-234321"
+        email="Mr.Doggy"
+      />
     </div>
 
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
+
+    console.log(props);
 
     const imgPath = process.env.PUBLIC_URL;
-
     return(
-
-        <div className="card">
-             <img  className="card-image" src={ imgPath + '/images/i3.svg'} /> 
-             <div className="cardStars">
-                <img  className="star" src={ imgPath + '/images/star.svg'} />
-                <span>5.0 </span>
-                <span className="grey"> (6). USA</span>
-             </div>
-             <p>Life lessons with Katie Zaferes</p>
-             <h2>From $36 / Person</h2>
-
+        <div className="cardContent">
+            <div className="card02">
+                <img  className="" src={ imgPath + props.image} /> 
+                <h3>{props.name}</h3>
+                <div className="info-group">
+                    <i class="fa fa-phone" aria-hidden="true"></i>
+                    <p>{props.phone}</p>
+                </div>
+                <div className="info-group">
+                    <p>{props.email}</p>
+                </div>                
+         </div>
         </div>
     );
-
 }
 
 export default Card;
