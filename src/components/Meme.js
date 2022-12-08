a4d43517643f8ead1f92ef1585c77cd5f40cb9b6
Dumindu Lakshan <it21266096@my.sliit.lk>
2022-12-08 20:42:39 +0530

    updated

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
+import MemeData from './memeData';
+
 const Meme = () => {
+
+    let url;
+
+    const getMeme = () => {
+        const memesArray = MemeData.data.meme;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        // alert(randomNumber);
+        url = memesArray[randomNumber].url;
+        alert(url); 
+    }
 
     return(
 
         <main>
-            <form  className="form">
+            <div className="meme">
                 <input type="text" placeholder="topText"/>
                 <input type="text" placeholder="bottomText"/>
-                <button  onClick>Get new Meme</button>
-            </form>
+                <button
+                    onClick={getMeme}
+                >Get new Meme</button>
+            </div>
         </main>
 
     );
 
 }
 
-export default Form;
+export default Meme;
---
+++ src/components/memeData.js
@@ -0,0 +1,39 @@
+export default {
+    "success": true,
+    "data": {
+        meme: [
+            {
+                "id": "181913649",
+                "name": "Dark Hotline Bling",
+                "url": "m1.jpg",
+                "width": 1200,
+                "height": 1200,
+                "box-count": 2
+            },
+            {
+                "id": "87743020",
+                "name": "Two Idiots",
+                "url": "m2.jpg",
+                "width": 1200,
+                "height": 1200,
+                "box-count": 1
+            },
+            {
+                "id": "1542433432",
+                "name": "Dark Hotline Bling",
+                "url": "m3.jpg",
+                "width": 1200,
+                "height": 1200,
+                "box-count": 2
+            },
+            {
+                "id": "3423423434",
+                "name": "Distracted",
+                "url": "m4.jpg",
+                "width": 1200,
+                "height": 1200,
+                "box-count": 2
+            }
+        ]
+    }
+}
