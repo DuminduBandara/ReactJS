3162ccaa67caac420f3ee25a7209d4adf96622a6
Dumindu Lakshan <it21266096@my.sliit.lk>
2022-12-08 21:08:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import Header from './components/Header';
-import Meme from './components/Meme';
-
 
 const App = () => {
+
+  const [data, setData] = React.useState(["array01", "array02"]);
+
+  
+  const addItem = () => {
+    const dataText = `array${data.length + 1}`;
+    setData(prevState => [...prevState, dataText])
+  }
+  const getArray = data.map(data => <p key={data}>{data}</p>);
+
   return(
 
     <div className="app">
-      <Header/>
-      <Meme/>
+      <button onClick={addItem}>Add Array</button>
+      {getArray}
     </div>
 
   );
