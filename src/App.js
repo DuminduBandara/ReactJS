f5439c63a1a69cc072dd7c0cd39a16b870be5309
Dumindu Lakshan <it21266096@my.sliit.lk>
2022-12-09 22:19:13 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ const App = () => {
 
   const [squares, setSquares] = React.useState(boxes)
 
-
+  const toggle = (id) => {
+    setSquares(prevSquare => {
+      return prevSquare.map((square) => {
+        return square.id === id ? {...square, on: !square.on} : square
+      })
+    })
+  }
 
   const squareElement = squares.map(square => (
-    <BoxM key={square.id} on={square.on}/>
+    <BoxM key={square.id} on={square.on} toggle={() => toggle(square.id)}/>
   )) //if you add html inside function make sure to add '()' not '{}'
 
   return(
