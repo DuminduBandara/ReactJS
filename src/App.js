66d522b82c9a2ec7b046ac602c61a86e1b904247
Dumindu Lakshan <it21266096@my.sliit.lk>
2022-12-09 22:04:28 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,10 +7,28 @@ const App = () => {
 
   const [squares, setSquares] = React.useState(boxes)
 
+  const toggle = (id) => {
+    setSquares(prevSquare => {
+      const newSquare = []
+      for(let i = 0; i < prevSquare.length; i++){
+        const currentSquare = prevSquare[i]
+        if(currentSquare.id === id){
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on
+          }
+          newSquare.push(updatedSquare)
+        }else {
+          newSquare.push(currentSquare)
+        }
+      }
 
+      return newSquare
+    })
+  }
 
   const squareElement = squares.map(square => (
-    <BoxM key={square.id} on={square.on}/>
+    <BoxM key={square.id} id={square.id} on={square.on} toggle={toggle}/>
   )) //if you add html inside function make sure to add '()' not '{}'
 
   return(
--- src/components/BoxM.js
+++ src/components/BoxM.js
@@ -2,21 +2,19 @@ import React from 'react';
 
 const BoxM = (props) => {
 
-    const [on,setOn] = React.useState(props.on)
+
    
     const styles = {
-        backgroundColor: on ? "#000" : "#fff"
+        backgroundColor: props.on ? "#000" : "#fff"
     }
 
 
-    const toggle = () => {
-        setOn(pervOn=> !pervOn)
-    }
+
 
     return (
         
         <div>
-            <div style={styles} className="box" onClick={toggle}></div>
+            <div style={styles} className="box" onClick={() => props.toggle(props.id)}></div>
         </div>
     )
 
