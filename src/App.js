37c170671a289f5c9e89fe0d3c31fedd362944ca
Dumindu Lakshan <it21266096@my.sliit.lk>
2022-12-10 20:36:16 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import React from 'react';
 const App = () => {
 
   const [useData, setUseData] = React.useState({})
+  const [count, setCount] = React.useState(1)
 
-  fetch("http://swapi.dev/api/people/1")
-  .then(res => res.json())
-  .then(data => setUseData(data))
+  React.useEffect(() => {
+    fetch(`http://swapi.dev/api/people/${count}`)
+    .then(res => res.json())
+    .then(data => setUseData(data))
+
+  }, [count])
+
 
   return(
     <div>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
       <pre>{JSON.stringify(useData, null, 2)}</pre>
     </div>
   );
