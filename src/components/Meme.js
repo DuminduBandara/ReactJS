dabcb44de7cc1b381000bb5e304e5684f38b4cb5
Dumindu Lakshan <it21266096@my.sliit.lk>
2022-12-10 21:41:15 +0530

    updated

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -18,9 +18,14 @@ const Meme = () => {
     const [allMemeImages, setAllMemeImages] = React.useState([]);
 
     React.useEffect( () => {
-        fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json())
-        .then(data => setAllMemeImages(data.data.memes))
+        async function getMeme() {
+
+            const res =  await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemeImages(data.data.memes)
+        }
+        getMeme()
+
     }, [])
 
     const getMeme = () => {
